67482cd0abb7c1aebc2115fbcf990c3127aea646
Antonio Marques <antoniomarquesfneto@gmail.com>
2025-06-05 19:51:32 -0300

    feat: implementa ajuste no modal e landingpage

--- src/app/demo/financial/pages/new-financial/new-financial.component.ts
+++ src/app/demo/financial/pages/new-financial/new-financial.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { fadeInOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-new-financial',
   standalone: true,
   imports: [],
   templateUrl: './new-financial.component.html',
-  styleUrl: './new-financial.component.scss'
+  styleUrl: './new-financial.component.scss',
+  animations: [fadeInOnEnterAnimation()]
 })
 export class NewFinancialComponent {
 
